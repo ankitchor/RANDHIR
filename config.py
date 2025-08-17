62aa2ef0e683295cf5b631bc09fa72b151b2d71b
ankitchor <randhirsingh95669@gmail.com>
2025-08-17 11:09:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ API_KEY = getenv("API_KEY", '30DxNexGenBots5f8ff9') # youtube song api ke
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Vipchor/Jani-Music-Papa-Repo",
+    "https://github.com/ankitchor/RANDHIR",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
@@ -120,3 +120,4 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
